5e2dce977fc060c53b8e2a85a1c0a051693b307a
Veera573026 <veeranjaneyulu704@gmail.com>
2024-11-13 22:38:34 -0600

    made changes

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 // Import dependencies
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var pickRouter = require('./routes/pick'); // Import pick.js route
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const pickRouter = require('./routes/pick');
 
-var app = express();
+const app = express();
 
 // View engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,20 +24,20 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Use routes
+// Route handlers
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/pick', pickRouter);
 
-// Route for grid page
+// Route for grid page with dynamic query parameters
 app.get('/grid', (req, res) => {
-  let query = req.query;
+  const query = req.query;
   console.log(`rows: ${query.rows}`);
   console.log(`cols: ${query.cols}`);
   res.render('grid', { title: 'Grid Display', query: query });
 });
 
-// Plants route (static data example)
+// Static route for displaying plants data
 app.get('/plants', (req, res) => {
   const results = [
     { plant_name: "Cactus", plant_type: "Succulent", plant_age: 5 },
@@ -48,22 +48,26 @@ app.get('/plants', (req, res) => {
 });
 
 // Catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // Error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
   res.status(err.status || 500);
   res.render('error');
 });
 
-// Connect to MongoDB
-console.log("MongoDB Connection String:", process.env.MONGO_CON);
-mongoose.connect(process.env.MONGO_CON)
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((error) => console.error("MongoDB connection error:", error));
+// MongoDB connection
+const mongoUri = process.env.MONGO_CON;
+if (!mongoUri) {
+  console.error("MongoDB connection string is missing in the .env file");
+} else {
+  mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("Connected to MongoDB"))
+    .catch(error => console.error("MongoDB connection error:", error));
+}
 
 module.exports = app;
