a8cfb6a391f69662b28517a8ca479ce97ed851c4
Veera573026 <veeranjaneyulu704@gmail.com>
2024-11-24 08:50:33 -0600

    Updated routes

--- routes/pick.js
+++ routes/pick.js
@@ -1,19 +1,9 @@
-<<<<<<< HEAD
 var express = require('express');
 var router = express.Router();
 
 /* GET Random Item page. */
 router.get('/', function(req, res) {
   res.render('randomitem', { title: 'A Random Item' });
-=======
-// routes/pick.js
-const express = require('express');
-const router = express.Router();
-
-// GET route to render the randomitem.pug template
-router.get('/', (req, res) => {
-    res.render('randomitem', { title: 'A Random Item' });
->>>>>>> 7aed5c59f629a1cdde052b98f4532532eb01c877
 });
 
 module.exports = router;
