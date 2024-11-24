d208b6c749a31fcf7112e9b929ebfa81cc1916b6
Veera573026 <veeranjaneyulu704@gmail.com>
2024-11-23 21:38:04 -0600

    Resolved conflict

--- models/account.js
+++ models/account.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-<<<<<<< HEAD
 const Schema = mongoose.Schema;
 const passportLocalMongoose = require("passport-local-mongoose");
 const accountSchema = new Schema({
@@ -10,19 +9,3 @@ accountSchema.plugin(passportLocalMongoose);
 // We export the Schema to avoid attaching the model to the
 // default mongoose connection.
 module.exports = mongoose.model("Account", accountSchema);
-=======
-const passportLocalMongoose = require('passport-local-mongoose');
-const Schema = mongoose.Schema;
-
-// Define account schema
-const accountSchema = new Schema({
-  username: String,
-  password: String
-});
-
-// Use passport-local-mongoose for authentication-related fields and methods
-accountSchema.plugin(passportLocalMongoose);
-
-// Export the model
-module.exports = mongoose.model('Account', accountSchema);
->>>>>>> 7aed5c59f629a1cdde052b98f4532532eb01c877
