156b39df830f5ea46446a23d092cea9fe39c75cf
Veera573026 <veeranjaneyulu704@gmail.com>
2024-11-23 21:38:39 -0600

    Resolved conflict

--- routes/grid.js
+++ routes/grid.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-<<<<<<< HEAD
 /* GET Grid page.*/
 router.get('/', function(req, res) {
     const rows = parseInt(req.query.rows, 10);
@@ -12,13 +11,6 @@ router.get('/', function(req, res) {
 
 //   res.render('grid', { title: "Grid Display", query: query });
   res.render('grid', { query: { rows, cols } });
-=======
-router.get('/', function(req, res) {
-    let query = req.query;
-    console.log(`rows ${query.rows}`);
-    console.log(`cols ${query.cols}`);
-    res.render('grid', { title: "Grid Display", query: query });
->>>>>>> 7aed5c59f629a1cdde052b98f4532532eb01c877
 });
 
 module.exports = router;
